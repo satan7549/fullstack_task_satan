314f8844390585a688efaa1b543b48d58e14dbb0
satan sharma <137980612+satan-furation@users.noreply.github.com>
2024-07-25 21:30:52 +0530

    icon and config change

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        customBrown: "rgb(146, 89, 37)",
+      },
+    },
+  },
+  plugins: [],
+};
